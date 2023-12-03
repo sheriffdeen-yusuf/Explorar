300d829b4c5aec39f853a0e6d1d92208f7181557
unknown <lilydamisa@gmail.com>
2023-12-03 17:38:49 +0100

    Created Car rentals page

--- src/pages/carrents/index.tsx
+++ src/pages/carrents/index.tsx
@@ -1,8 +1,39 @@
 import AppLayout from "@/components/AppLayout";
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
 
 function index() {
-  return <AppLayout>Car Rentals</AppLayout>;
+  return (
+    // <AppLayout>Car Rentals</AppLayout>,
+    <div className="flex  gap-8 flex-col items-center ">
+      {/* <Image
+        src="/carrentals.svg"
+        className="pt-1 text-center"
+        alt=""
+        width={430}
+        height={261}
+      /> */}
+      <div className="w-full h-80 text-center items-center">
+        <h1 className="bg-[url('/carrentals.svg')] h-full bg-cover bg-no-repeat bg-center text-white pt-20">Car Rentals</h1>
+      </div>
+      
+      <Link href="/">
+        <button className="button-minor">Date of rental</button>
+      </Link>
+      <Link href="/">
+        <button className="button-minor">Name of company</button>
+      </Link>
+      <Link href="/">
+        <button className="button-minor">Type of vehicle</button>
+      </Link>
+      <Link href="/" className="mt-14">
+        <button className="button-minor">Save</button>
+      </Link>
+
+    </div>
+          )
 }
 
 export default index;
--- src/pages/filghts/index.tsx
+++ src/pages/filghts/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 
 function index() {
   return <AppLayout>Flights</AppLayout>;
+  <img src="./" alt="" />
 }
 
 export default index;
